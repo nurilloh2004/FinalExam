3bca1c599b110f6d77f9593dcf15e946a76b9f80
nurilloh.2004 <nurillohpython@gmail.com>
2022-04-21 14:26:44 +0500

    *

--- settings.py
+++ settings.py
@@ -2,3 +2,9 @@ import os
 
 base_path = os.getcwd()
 db_path = os.path.join(base_path, 'db\db.db')
+
+ip = 'localhost'
+port = 5432
+user_name = 'postgres'
+password = 'zxcvasdg'
+db_name = 'postgres'
--- test.py
+++ test.py
@@ -2,17 +2,17 @@ from settings import *
 import psycopg2
 
 conn = psycopg2.connect(
-    host=ip,
-    port=port,
+    f"dbname = {db_name} user = {user_name} password = {password}"
+)
 
+conn = psycopg2.connect(
+    host=ip,
     database=db_name,
     user=user_name,
     password=password)
 
 cursor = conn.cursor()
-cursor.execute("Insert into region (name) values \
-('Toshkent shahri'),\
-('Toshkent viloyati');")
+cursor.execute("Select * from market")
 
 for row in cursor:
-    print(*row)
+    print(*row, sep='|')
